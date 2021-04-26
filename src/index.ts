f4bde93db060566dd4b9828744f5762d157515e6
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-04-26 12:45:51 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.85.0/http/server.ts";
 
-let port = parseInt(Deno.env.get("PORT") ?? "8000")
+// let port = parseInt(Deno.env.get("PORT") ?? "8000")
+let port = 8081;
 const s = serve({ port });
 
 console.log(`http://localhost:${port}/`);
